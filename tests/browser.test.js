8166df294760abd6afe6041162fed704f9a877df
PyA99 <py.altrell@telia.com>
2026-05-13 12:35:04 +0200

    La till ett misslyckat jest test

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -20,17 +20,17 @@ afterAll(async() => {
     await driver.quit();
 }, defaultTimeout);
 
-test('The stack should be empty in the beginning', async () => {
-    let stack = await driver.findElement(By.id('top_of_stack')).getText();
-    expect(stack).toEqual("n/a");
-});
+// test('The stack should be empty in the beginning', async () => {
+//     let stack = await driver.findElement(By.id('top_of_stack')).getText();
+//     expect(stack).toEqual("n/a");
+// });
 
-describe('Clicking "Pusha till stacken"', () => {
-    it('should open a prompt box', async () => {
-        let push = await driver.findElement(By.id('push'));
-        await push.click();
-        let alert = await driver.switchTo().alert();
-        await alert.sendKeys("Bananer");
-        await alert.accept();
-    });
-});
+// describe('Clicking "Pusha till stacken"', () => {
+//     it('should open a prompt box', async () => {
+//         let push = await driver.findElement(By.id('push'));
+//         await push.click();
+//         let alert = await driver.switchTo().alert();
+//         await alert.sendKeys("Bananer");
+//         await alert.accept();
+//     });
+// });
