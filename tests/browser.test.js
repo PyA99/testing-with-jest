ee6057b2741f264831f3f66e1ac20e7122ba08d4
PyA99 <py.altrell@telia.com>
2026-05-25 17:07:44 +0200

    La tillbaka de andra testerna som jag innan avkommenterade och körde testet igen

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -69,21 +69,18 @@ test('pop button removes top element', async () => {
     expect(stackText).toEqual('n/a');
 });
 
+test('The stack should be empty in the beginning', async () => {
+    let stack = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(stack).toEqual("n/a");
+});
 
-
-
-// test('The stack should be empty in the beginning', async () => {
-//     let stack = await driver.findElement(By.id('top_of_stack')).getText();
-//     expect(stack).toEqual("n/a");
-// });
-
-// describe('Clicking "Pusha till stacken"', () => {
-//     it('should open a prompt box', async () => {
-//         let push = await driver.findElement(By.id('push'));
-//         await push.click();
-//         let alert = await driver.switchTo().alert();
-//         await alert.sendKeys("Bananer");
-//         await alert.accept();
-//     });
-// });
+describe('Clicking "Pusha till stacken"', () => {
+    it('should open a prompt box', async () => {
+        let push = await driver.findElement(By.id('push'));
+        await push.click();
+        let alert = await driver.switchTo().alert();
+        await alert.sendKeys("Bananer");
+        await alert.accept();
+    });
+});
 
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,22 +1,22 @@
 const stack = require('../src/stack');
 
-// test('peek on empty stack returns undefined', () => {
-//     expect(stack.peek()).toBeUndefined();
-// });
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
 
-// test('peek on stack with one element returns that element', () => {
-//     stack.push(1);
-//     expect(stack.peek()).toBeDefined();
-//     expect(stack.peek()).toBe(1);
-// });
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
 
-// test('peek on stack with two or more elements returns the top element', () => {
-//     stack.push(1);
-//     stack.push("wow");
-//     stack.push(42);
-//     expect(stack.peek()).toBeDefined();
-//     expect(stack.peek()).toBe(42);
-// });
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
+});
 
 test('pop returns the latest pushed value', () => {
     stack.push('äpple');
